fa890de7daf116e8b3be529f5c4f25ca4c21dc19
Ochomba Obadiah <oochomba@gmail.com>
2024-09-26 13:05:56 +0300

    final commit

--- vue/resources/views/partials/placebid.blade.php
+++ vue/resources/views/partials/placebid.blade.php
@@ -22,7 +22,7 @@
 						</div>
 						<div class="{{ $errors->has('offer') ? ' has-error' : '' }}">
 							<label for="offer" class="lable">Bid Message</label>
-							<textarea    class="form-control " name="offer" value="{{ old('offer') }}" rows="3" maxlength="300" required="true"></textarea>
+							<textarea    class="form-control " name="offer" value="{{ old('offer') }}" rows="3" maxlength="300" required></textarea>
 							@if ($errors->has('offer'))
 								<span class="help-block"><strong>{{ $errors->first('offer') }}</strong></span>
 							@endif
